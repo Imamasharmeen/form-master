e2c9d5c5515757f766d3658d6567a7f635f960a8
Imamasharmeen <imamasharmeen@gmail.com>
2024-10-30 17:13:39 +0200

    Added basic structure

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SimpleForm() {
+  return (
+    <div>
+        <form>
+            <input type="text" placeholder="input"></input>
+            <br></br>
+            <button>Submit</button>
+        </form>
+    </div>
+  )
+}
